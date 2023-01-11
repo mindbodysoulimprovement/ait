4ec6450631994465b64513a654551f85deac1e57
mohelt <mindbodysoulimprovement@gmail.com>
2023-01-11 14:03:49 +0000

    Fixing heroku deploy #2

--- server/server.js
+++ server/server.js
@@ -8,17 +8,17 @@ dotenv.config()
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-app.get('/', async (req, res) => {
-  res.status(200).send({
-    message: 'Hello from aitrainer.co!'
-  })
-})
+
 const openai = new OpenAIApi(configuration);
 
 const app = express()
 app.use(cors())
 app.use(express.json())
-
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from aitrainer.co!'
+  })
+})
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
